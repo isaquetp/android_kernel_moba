57cfe70d4dd4251f00493fc923f890f62eaf1757
dread <dreadcreator5.com>
2023-08-21 14:47:28 +0530

    build.sh: add symlink for internal.h
    Signed-off-by: dread <dreadcreator5.com>

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ export PATH="$HOME/toolchains/proton-clang/bin:$PATH"
 
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+ln -s ../../../../drivers/base/regmap/internal.h techpack/audio/include/soc/internal.h
+ln -s ../../../drivers/pinctrl/core.h techpack/audio/soc/core.h
+ln -s ../../../drivers/pinctrl/pinctrl-utils.h techpack/audio/soc/pinctrl-utils.h
