cefaf513edd23a27dea4646a8dfe7e65880dc080
taotaoS2 <superboydbr@gmail.com>
2023-11-01 21:11:43 -0300

    moba: remove unnecessary things and fix kernel

--- kfix.sh
+++
@@ -1,7 +0,0 @@
-rm -f techpack/audio/include/soc/internal.h
-rm -f techpack/audio/soc/core.h
-rm -f techpack/audio/soc/pinctrl-utils.h
-
-ln -s ../../../../drivers/base/regmap/internal.h techpack/audio/include/soc/internal.h
-ln -s ../../../drivers/pinctrl/core.h techpack/audio/soc/core.h
-ln -s ../../../drivers/pinctrl/pinctrl-utils.h techpack/audio/soc/pinctrl-utils.h
